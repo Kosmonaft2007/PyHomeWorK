f19377b22a455806739f56caa85e8c72a8645432
Vadim <kosmonaft2007@ya.ru>
2022-09-24 16:33:59 +0300

    firat commit

---
+++ HomeWork/Test.py
@@ -0,0 +1,17 @@
+
+# int
+namber = 10
+
+# float
+price = 100.5
+
+# str
+name = 'Adraham'
+
+# bool
+status = False
+
+print(namber)
+
+print('Test')
+
---
+++ Lecture/lec.py
@@ -0,0 +1,2 @@
+print ('prinrrrrt')
+print('jlknsn')
